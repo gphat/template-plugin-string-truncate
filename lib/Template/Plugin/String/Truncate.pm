28639044932670844629e55e006a856c120fb8a3
Cory G Watson <gphat@onemogin.com>
2011-07-07 18:40:14 -0500

    Updated docs.

--- lib/Template/Plugin/String/Truncate.pm
+++ lib/Template/Plugin/String/Truncate.pm
@@ -12,20 +12,26 @@ use String::Truncate qw();
 
     [% USE st = String.Truncate %]
 
-    This thing happened [% time_dir.ago(some_seconds) %].
+    This string is elided: [% st.elide('LONG ASS STRING, SON!', 18); %]
+    This one is truncated: [% st.trunc('DAMN YOU GOT LONG STRINGS!', 10); %]
+    And other stuff can happen too: [% st.elide('SHORT STRING', 5, { truncate => 'left' }) %]
 
 =head1 DESCRIPTION
 
 This plugin allows you to use functions from L<String::Truncate> in your templates.
-It is very simple and hopefully requires little explanation.
+It is very simple and hopefully requires little explanation.  
 
 =function elide
+
+Truncates a string and marks the elision.  See L<String::Truncate/elide>.
 
 =cut
 
 sub elide { shift; return String::Truncate::elide(@_) }
 
-=function trun
+=function trunc
+
+Truncates a string.  See L<String::Truncate/trunc>.
 
 =cut
 
